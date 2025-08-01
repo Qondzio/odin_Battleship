20922a64f4b89603ceb658ca14afe0762784d6db
Qondzio <qondziolabaziewicz@gmail.com>
2025-08-01 15:27:50 +0200

    Added an option to choose a length of the placed ship

--- control.js
+++ control.js
@@ -6,3 +6,21 @@ const game=gameboard()
 document.getElementById('rotate').addEventListener('click', ()=>{
     (game.getRotate()===false)? game.setRotate(true):game.setRotate(false);
 })
+
+//Select length of places ships
+let selectedLength=5;
+
+function getSelectedLength(){
+    return selectedLength;
+}
+export {getSelectedLength}
+
+document.querySelectorAll('.selectShip').forEach(element =>[
+    element.addEventListener('click', ()=>{
+        selectedLength=element.querySelector('.shipPreview').childElementCount;
+        document.querySelectorAll('.selectShip').forEach(item =>{
+            item.classList.remove('selectedShip')
+        })
+        element.classList.toggle('selectedShip')
+    })
+])
