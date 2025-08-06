581a3e120a31a26c382576bc5e4c32b38716524c
Qondzio <qondziolabaziewicz@gmail.com>
2025-08-06 20:25:12 +0200

    Added a function which allows a player to start a game only when he placed all of his ships. Also added a function which prepares a GUI for a player and starts a game.

--- control.js
+++ control.js
@@ -1,12 +1,11 @@
 import gameboard from "./gameboard.js";
 
 const game=gameboard()
-
 //Change rotation while placing ships
 document.getElementById('rotate').addEventListener('click', ()=>{
+    game.generateComputerPosition();
     (game.getRotate()===false)? game.setRotate(true):game.setRotate(false);
 })
-
 //Select length of places ships
 let selectedLength=5;
 
@@ -28,8 +27,6 @@ document.querySelectorAll('.selectShip').forEach(element =>[
 //Make a div 'blank' and unable to choose once a ship has been placed
 const selectedLengths=[];
 function blankDiv(shipLength){
-    console.log(shipLength);
-    
     document.querySelectorAll('.selectShip').forEach(div =>{
         if(div.querySelector('.shipPreview').childElementCount === shipLength){
             div.classList.remove('selectedShip')
@@ -54,9 +51,45 @@ function blankDiv(shipLength){
             div.classList.toggle('disabledRow')
         })
         document.getElementById('rotate').classList.toggle('disabledDiv')
-        document.getElementById('start').style.backgroundColor='rgba(21, 190, 21, 0.72)';
-        document.getElementById('start').style.border='1px solid rgba(21, 190, 21, 0.72)';
+        document.getElementById('start').style.backgroundColor='rgba(47, 223, 47, 0.72)';
+        document.getElementById('start').style.border='1px solid rgba(47, 223, 47, 0.72)';
+        
     }
+}
+
+
+//Allows to start a game only if player has placed all of his ships
+document.getElementById('start').addEventListener('click', ()=>{
+    if(selectedLengths.length === 5){
+        startGame();
+    }
+})
+
+
+function startGame(){
+
+    (function prepareGameGui(){
+        document.querySelector('.choose').style.display='none';
+        document.querySelector('.computerGameboard').style.display='grid';
+        document.querySelector('.info').style.display='block';
+        document.querySelector('.gameInfo').textContent='Your turn!'
+        document.querySelectorAll('.boardInfo').forEach(div =>{
+            div.style.display='block';
+        });
+
+        for(let i=1;i<=10;i++){
+            for(let j=1;j<=10;j++){
+                const div=document.createElement('div');
+                div.classList.add('row');
+                div.setAttribute('row',i);
+                div.setAttribute('column',j);
+
+                document.querySelector('.computerGameboard').appendChild(div);
+            }
+        }
+
+        game.generateComputerPosition();
+    })()
 }
 
 export {getSelectedLength, blankDiv}
